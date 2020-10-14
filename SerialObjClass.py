e07716b6a56be5baad9e766be74f381e5b72af86
Ian Hong <ianqyhong@gmail.com>
2020-10-14 19:54:14 -0400

    updated

--- SerialObjClass.py
+++ SerialObjClass.py
@@ -1,4 +1,63 @@
 import serial
-import PyQt5
 
-print('success')
+
+class SerialObj:
+    def __init__(self, COMPort, baudRate=9600):
+        self.COMPort = COMPort
+        self.baudRate = baudRate
+
+        self.serialDevice = serial.Serial(self.COMPort, self.baudRate)
+
+    
+    def serialRead(self):
+        self.rawBytes = self.serialDevice.read(24)
+
+        return self.rawBytes
+
+
+    def decodeBytes(self, rawBytes):
+        try:
+            self.byteToStr = rawBytes.decode('utf-8')
+            self.decodeable = True
+            return self.byteToStr
+        except:
+            self.decodeable = False
+
+
+    def regExMatching(self, byteToStr):
+
+        regExPattern = re.compile(r'(X)(+|-)(\d\d)(\d\d)')
+        
+        self.matchObj = regExPattern.search(byteToStr)
+
+        if matchObj != None:
+            return self.matchObj.groups()
+        else:
+            print('No match found, processing next string of bytes')
+
+    
+    def reverseSign(self, readingSign):
+        if readingSign == '+':
+            readingSign = '-'
+        elif readingSign == '-':
+            readingSign = '+'
+
+        return readingSign
+
+
+    def translate(self):
+        while self.decodeable is not True:
+            self.rawBytes = self.serialRead()
+            self.byteToStr = self.decodeBytes(self.rawBytes)
+
+        self.readingGrps = self.regExMatching(self.byteToStr)
+
+        self.readingSign = reverseSign(self.readingGrps[1])
+        self.whole = int(self.readingGrps[2])
+        self.dec = round(int(self.readingGrps[3]/100), 1)
+
+        self.numReading = self.whole + self.dec
+
+        self.finalReading = self.readingSign + str(self.numReading)
+
+        return [self.readingSign, self.numReading, self.finalReading]
--- readingUI.py
+++ readingUI.py
@@ -17,8 +17,8 @@ class readingUI(QMainWindow):
         self.defaultPath = 'T:\\Engineering\Enginnering\Projects'
 
         self.setWindowTitle('DXL360 Readings')
-        self.setFixedSize(400, 300)
-        self.setGeometry(300, 200, 0, 0)
+        self.setFixedSize(600, 500)
+        self.move(400, 200)
         self._centralWidget = QWidget(self)
 
         self.layout = QGridLayout()
@@ -28,7 +28,8 @@ class readingUI(QMainWindow):
 
         self.createInputLayout()
         self.createReadButton()
-        # self.signals()
+        self.createStartButton()
+        self.createClearButton()
         
 
     def createInputLayout(self):
@@ -36,53 +37,69 @@ class readingUI(QMainWindow):
         # ----- Initialize input layout ----- #
 
         self._inputLayout = QGridLayout()
-        # self._inputLayout.setGeometry(QRect(QPoint(0, 0), QSize(50,50)))
-        self._inputLayout.width = 100
-        self._inputLayout.height = 100
 
 
         # ----- Assign elements to variables so we can access them later ----- #
 
-        lbCOMPort = QLabel('COM Port:')
-        lbDate = QLabel('Date: ')
-        lbFilePath = QLabel('Output file path:')
-        lbFileName = QLabel('Output file name:')
+        self.lbCOMPort = QLabel('COM Port:')
+        self.lbDate = QLabel('Date:')
+        self.lbArmID = QLabel('Arm ID:')
+        self.lbFilePath = QLabel('Output file path:')
+        self.lbFileName = QLabel('Output file name:')
 
-        edCOMPort = QLineEdit()
-        edDate = QLineEdit(
+        self.edCOMPort = QLineEdit()
+        self.edDate = QLineEdit(
             str(datetime.now().strftime('%b')) +
             '. ' +
             str(datetime.now().day) +
             ', ' +
             str(datetime.now().year)
             )
-        edFilePath = QLineEdit('T:\\Engineering\\Engineering\\Projects')
-        edFileName = QLineEdit('readings.xlsx')
+        self.edArmID = QLineEdit(str(datetime.now()))
+        self.edFilePath = QLineEdit('T:\\Engineering\\Engineering\\Projects')
+        self.edFileName = QLineEdit('readings.xlsx')
 
 
         # ----- Initialize input form elements, set default values ----- #
 
-        self._inputLayout.addWidget(lbCOMPort, 0, 0)
-        self._inputLayout.addWidget(edCOMPort, 0, 1)
+        self._inputLayout.addWidget(self.lbCOMPort, 0, 0)
+        self._inputLayout.addWidget(self.edCOMPort, 0, 1)
 
-        self._inputLayout.addWidget(QLabel('Date:'), 1, 0)
-        self._inputLayout.addWidget(QLineEdit(
-            str(datetime.now().strftime('%b')) +
-            '. ' +
-            str(datetime.now().day) +
-            ', ' +
-            str(datetime.now().year)
-            ),
-            1, 1)
+        self._inputLayout.addWidget(self.lbDate, 1, 0)
+        self._inputLayout.addWidget(self.edDate, 1, 1)
 
-        self._inputLayout.addWidget(QLabel('Output file path:'), 2, 0)
-        self._inputLayout.addWidget(
-            QLineEdit('T:\\Engineering\\Engineering\\Projects'), 2, 1)
+        self._inputLayout.addWidget(self.lbArmID, 2, 0)
+        self._inputLayout.addWidget(self.edArmID, 2, 1)
 
-        self._inputLayout.addWidget(lbFileName, 3, 0)
-        self._inputLayout.addWidget(edFileName, 3, 1)
+        self._inputLayout.addWidget(self.lbFilePath, 3, 0)
+        self._inputLayout.addWidget(self.edFilePath, 3, 1)
 
-        edCOMPort.setCursorPosition(5)
+        self._inputLayout.addWidget(self.lbFileName, 4, 0)
+        self._inputLayout.addWidget(self.edFileName, 4, 1)
+
+        # ----- Format input form elements ----- #
+        self.lbCOMPort.setAlignment(Qt.AlignRight)
+        self.lbDate.setAlignment(Qt.AlignRight)
+        self.lbArmID.setAlignment(Qt.AlignRight)
+        self.lbFilePath.setAlignment(Qt.AlignRight)
+        self.lbFileName.setAlignment(Qt.AlignRight)
+
+        self.edCOMPort.setFixedWidth(100)
+        self.edCOMPort.setAlignment(Qt.AlignTop)
+
+        self.edDate.setFixedWidth(200)
+        self.edDate.setAlignment(Qt.AlignTop)
+
+        self.edArmID.setFixedWidth(200)
+        self.edArmID.setAlignment(Qt.AlignTop)
+        self.edArmID.setCursorPosition(0)
+
+        self.edFilePath.setFixedWidth(200)
+        self.edFilePath.setAlignment(Qt.AlignTop)
+        self.edFilePath.setCursorPosition(0)
+        
+        self.edFileName.setFixedWidth(200)
+        self.edFileName.setAlignment(Qt.AlignTop)
 
 
         # ----- Insert layouts into appropriate spots ----- #
@@ -90,23 +107,62 @@ class readingUI(QMainWindow):
         self.layout.addLayout(self._inputLayout, 0, 0)
 
 
+    # ----- Create some buttons ----- #
+
+    def createStartButton(self):
+        self.startButton = QPushButton('Start')
+
+        self.layout.addWidget(self.startButton, 1, 0)
+        self.layout.itemAtPosition(1, 0).setAlignment(Qt.AlignRight)
+
+
+    def createClearButton(self):
+        self.clearButton = QPushButton('Clear')
+
+        self.layout.addWidget(self.clearButton, 1, 1)
+        self.layout.itemAtPosition(1, 1).setAlignment(Qt.AlignLeft)
+
+
     def createReadButton(self):
         
         self.readButton = QPushButton('Take reading')
         self.readButton.setFont((QFont('Calibri', 24)))
-        self.readButton.setFixedSize(300, 100)
+        self.readButton.setFixedSize(400, 100)
         self.readButton.hasFocus()
 
-        self.layout.addWidget(self.readButton, 1, 0)
+        self.layout.addWidget(self.readButton, 2, 0, 1, 2)
 
-        self.layout.itemAtPosition(1, 0).setAlignment(Qt.AlignCenter)
+        self.layout.itemAtPosition(2, 0).setAlignment(Qt.AlignHCenter)
 
 
-    def signals(self):
+    # ----- Define some methods ----- #
+
+    def clearInputs(self):
+        self.edCOMPort.setText('')
+        self.edDate.setText('')
+        self.edArmID.setText('')
+        self.edFilePath.setText('')
+        self.edFileName.setText('')
+
+    def isReading(self):
+        if self.startButton.text() == 'Start':
+            self.startButton.setText('Stop')
+        elif self.startButton.text() == 'Stop':
+            self.startButton.setText('Start')
+
+    def takeReading(self):
         pass
 
-class readingController:
-    pass
+
+class UIController:
+    def __init__(self, readingWindow):
+        self.readingWindow = readingWindow
+        self.connectSignals()
+
+
+    def connectSignals(self):
+        self.readingWindow.clearButton.clicked.connect(self.readingWindow.clearInputs)
+        self.readingWindow.startButton.clicked.connect(self.readingWindow.isReading)
 
     
 def main():
@@ -115,6 +171,8 @@ def main():
 
     readingWindow = readingUI()
     readingWindow.show()
+
+    UIController(readingWindow=readingWindow)
 
     sys.exit(DXL360App.exec_())
 
