8a5e4d79d366b12d3dcbd48cdc98f56d3f0c5f21
Ian Hong <ianqyhong@gmail.com>
2020-10-15 23:18:22 -0400

    added UI logic

--- readingUI.py
+++ readingUI.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from PyQt5.QtCore import Qt, QRect, QPoint, QSize
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
+from PyQt5.QtWidgets import QMenuBar
 from PyQt5.QtWidgets import QLabel, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem
 from PyQt5.QtWidgets import QFormLayout, QVBoxLayout, QHBoxLayout, QGridLayout
 
@@ -28,6 +29,8 @@ class readingUI(QMainWindow):
         self.setCentralWidget(self._centralWidget)
         self._centralWidget.setLayout(self.layout)
 
+        self.curRow = 0
+
         # Initialize UI elements
         self.createInputLayout()
         self.createTable()
@@ -37,6 +40,7 @@ class readingUI(QMainWindow):
 
         # Place UI elements into grid slots
         self.layoutElements()
+
 
     def createInputLayout(self):
 
@@ -92,6 +96,8 @@ class readingUI(QMainWindow):
         self.edArmID.setCursorPosition(0)
         self.edFilePath.setCursorPosition(0)
 
+        self.edFilePath.setToolTip(self.edFilePath.text())
+
     
     def createTable(self):
         self.defaultArmPos = [
@@ -115,19 +121,41 @@ class readingUI(QMainWindow):
     def createStartButton(self):
         self.startButton = QPushButton('Start')
         self.startButton.setFixedHeight(50)
+        self.startButton.setFont(QFont('Calibri', 16))
 
 
     def createClearButton(self):
         self.clearButton = QPushButton('Clear')
         self.clearButton.setFixedHeight(50)
+        self.clearButton.setFont(QFont('Calibri', 16))
 
 
     def createReadButton(self):
-        
         self.readButton = QPushButton('Take reading')
         self.readButton.setFont((QFont('Calibri', 24)))
         self.readButton.setFixedSize(200, 100)
-        # self.readButton.hasFocus()
+
+    
+    def layoutElements(self):
+        self.layout.addLayout(self._inputLayout, 0, 0)
+
+        # --- Layout start/clear buttons --- #
+        self.vertLayout1 = QVBoxLayout()
+
+        self.vertLayout1.addWidget(self.startButton, 0)
+        self.vertLayout1.itemAt(0).setAlignment(Qt.AlignRight)
+
+        self.vertLayout1.addWidget(self.clearButton, 1)
+        self.vertLayout1.itemAt(1).setAlignment(Qt.AlignRight)
+
+        self.layout.addLayout(self.vertLayout1, 0, 1)
+        # ----------------------------------- #
+
+        self.layout.addWidget(self.readingTbl, 3, 0)
+        self.layout.itemAtPosition(3,0).setGeometry(QRect(0, 0, 50, 50))
+
+        self.layout.addWidget(self.readButton, 3, 1, 1, 2)
+        self.layout.itemAtPosition(3, 0).setAlignment(Qt.AlignHCenter)
 
 
     # ----- Define some methods ----- #
@@ -141,6 +169,8 @@ class readingUI(QMainWindow):
         
         for row in range(self.readingTbl.rowCount()):
             self.readingTbl.setItem(row, 1, QTableWidgetItem(''))
+            self.curRow = 0
+            self.readingTbl.setCurrentCell(0, 1)
 
 
     def isReading(self):
@@ -150,26 +180,17 @@ class readingUI(QMainWindow):
         elif self.startButton.text() == 'Stop':
             self.startButton.setText('Start')
 
-    
-    def layoutElements(self):
-        self.layout.addLayout(self._inputLayout, 0, 0)
 
-        # --- Layout start/clear buttons --- #
-        self.vertLayout1 = QVBoxLayout()
+    def takeReading(self):
+        if self.readingTbl.currentRow() == -1:
+            self.readingTbl.setCurrentCell(0, 1)
+            self.readingTbl.setItem(self.readingTbl.currentRow(), 1, QTableWidgetItem('success'))
+            self.readingTbl.setCurrentCell(self.readingTbl.currentRow() + 1, 1)
+        else:
+            self.readingTbl.setItem(self.readingTbl.currentRow(), 1, QTableWidgetItem('success'))
+            self.readingTbl.setCurrentCell(self.readingTbl.currentRow() + 1, 1)
 
-        self.vertLayout1.addWidget(self.startButton, 0)
-        # self.vertLayout1.itemAt(0).setAlignment(Qt.AlignCenter)
-
-        self.vertLayout1.addWidget(self.clearButton, 1)
-        # self.vertLayout1.itemAt(1).setAlignment(Qt.AlignCenter)
-
-        self.layout.addLayout(self.vertLayout1, 0, 1)
-        # ----------------------------------- #
-
-        self.layout.addWidget(self.readingTbl, 3, 0)
-
-        self.layout.addWidget(self.readButton, 3, 1, 1, 2)
-        self.layout.itemAtPosition(3, 0).setAlignment(Qt.AlignHCenter)
+        print('current row is ' + str(self.readingTbl.currentRow()))
 
 
 class UIController:
@@ -181,6 +202,7 @@ class UIController:
     def connectSignals(self):
         self.readingWindow.clearButton.clicked.connect(self.readingWindow.clearInputs)
         self.readingWindow.startButton.clicked.connect(self.readingWindow.isReading)
+        self.readingWindow.readButton.clicked.connect(self.readingWindow.takeReading)
 
     
 def main():
